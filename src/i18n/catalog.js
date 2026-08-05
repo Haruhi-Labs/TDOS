04c95b5e38e347636ab09bdcf64745f295b90692
Haruhiyuki <haruhiism15532@outlook.com>
2026-08-05 14:38:52 +0800

    拆分国际化词典模块

---
+++ src/i18n/catalog.js
@@ -0,0 +1,10 @@
+import { CHARACTER_TEXT } from "./character-text.js";
+import { EN_MESSAGES } from "./messages-en.js";
+import { JA_MESSAGES } from "./messages-ja.js";
+
+export { CHARACTER_TEXT };
+
+export const MESSAGES = {
+  ja: JA_MESSAGES,
+  en: EN_MESSAGES,
+};
